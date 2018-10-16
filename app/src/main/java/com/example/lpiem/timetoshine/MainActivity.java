8e5059a286ea4af14fe5bdbc8915546c9513bdd6
Noé Colin <noe_colin@yahoo.fr>
2018-10-16 16:32:46 +0200

    voyage de folie

--- app/src/main/java/com/example/lpiem/timetoshine/MainActivity.java
+++ app/src/main/java/com/example/lpiem/timetoshine/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lpiem.timetoshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    String voyage1 = "le voyage de ouf";
 }
