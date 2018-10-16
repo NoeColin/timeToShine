8febc4ce61e6655765477cf1ab3725e55815fe50
lpiem <lpiem@iutabgdin202-12.univ-lyon1.fr>
2018-10-16 16:31:54 +0200

    Add sorts functions

--- app/src/main/java/com/example/lpiem/timetoshine/MainActivity.java
+++ app/src/main/java/com/example/lpiem/timetoshine/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lpiem.timetoshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortByDate(){
+        Toast.makeText(this, "Trier par date", Toast.LENGTH_SHORT).show();
+    }
+
+    public void sortByName(){
+        Toast.makeText(this, "Trier par nom", Toast.LENGTH_SHORT).show();
     }
 }
